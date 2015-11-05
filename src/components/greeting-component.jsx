2bccbcd8a1f1eee07f513f2332b4f266a082f3be
Eric Baer <me@ericbaer.com>
2015-11-05 11:26:34 -0800

    Create formatted greeting component

--- src/components/greeting-component.jsx
+++ src/components/greeting-component.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+"use strict";
 
-export default (props) => {
+import React from "react";
+import {FormattedMessage} from "react-intl"
+import {intlShape, injectIntl} from 'react-intl';
+
+const Greeting = (props) => {
   return (
     <h1>
-      Hello there, {props.name}!
+      <FormattedMessage
+        id="greeting"
+        description="Welcome greeting to the user"
+        defaultMessage="Hello! How are you today?"
+        />
     </h1>
   );
 }
+
+Greeting.propTypes = {
+  intl: intlShape.isRequired
+};
+
+export default injectIntl(Greeting);
